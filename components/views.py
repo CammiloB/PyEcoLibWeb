00eaf88653ab62ee4159950dfc399e7e100975a1
CammiloBlanco <sergio.camilo.blanco@gmail.com>
2021-02-09 23:07:25 -0500

    Fix the index page and add the installation page

--- components/views.py
+++ components/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pyecolib/home/index.html')
+
+def installation(request):
+    return render(request, 'pyecolib/installation/installation.html')
